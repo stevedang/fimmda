c85d394f6f76ddf0fc56567ab698fe3bc94ce0df
nguyendd@gmail.com <sdang@murex.com>
2017-08-22 16:05:30 +0800

    new add files

--- fimmda/start_service.sh
+++ fimmda/start_service.sh
@@ -2,6 +2,7 @@
 unset PYTHONPATH
 unset PYTHONHOME
 
+##############################
 #check the os and assign the python commands
 OS_TYPE=`uname`
 if [ "$OS_TYPE" = "SunOS" ]; then
@@ -11,63 +12,85 @@ else
 	python_command="python.exe" #if this is cygwin
 	awk_command="awk" #if this is cygwin
 fi
+##############################
+config_parser()
+{
+	local search_string=$2
+	local properties_file=$1
+	local __resultvar=$2	
+	local myresult=$($awk_command -F "=" '/'$search_string'/ {sub(/^[ \t\r\n]+/, "", $2); sub(/[ \t\r\n]+$/, "", $2);print $2}' $properties_file)
+	eval $__resultvar="'$myresult'"
+}
 
-#the current working folder 
-BASEDIR=$(dirname $0)
-
-#time each loop in seconds, by default it is 60 seconds
-LOOPTIME=2
-
-#input folder where we scan and pick up the file
-input_folder="input"
-
-#output folder where we retrieve the transformed file
-output_folder="output"
-
-#the source folder where the main.py resides
-source_folder="./sources"
-
-# the log folder
-LOGFOLDER="logs"
-
-#log file to write out all the information
-LOGFILE="service.log"
-
-#result file
-RESULTFILE="output.log"
-
-#pid file to kill off the daemon 
-PIDFILE="service.pid"
-
-#filename
-filename=""
-
+##############################
 #config file
-CONFIGFILE="fimmda.properties"
+CONFIGFILE="sources/fimmda.properties"
 
-#MDIT folder and MDIT commands
-mdit_folder="../"
-mdit_command="./marketdataInterface.sh -f "
+load_variables()
+{
+	#the current working folder 
+	BASEDIR=$(dirname $0)
+	
+	#time each loop in seconds, by default it is 60 seconds
+	config_parser $CONFIGFILE LOOPTIME 
+	
+	#input folder where we scan and pick up the file
+	#input_folder="input"
+	config_parser $CONFIGFILE input_folder
+	#output folder where we retrieve the transformed file
+	#output_folder="output"
+	config_parser $CONFIGFILE output_folder
 
-######################################
-#check and create input folder if no exist
-[ ! -d $BASEDIR/$input_folder ] && { mkdir -p $BASEDIR/$input_folder; }
+	#the source folder where the main.py resides
+	#source_folder="./sources"
+	config_parser $CONFIGFILE source_folder 
 
-#check and create output folder if no exist
-[ ! -d $BASEDIR/$output_folder ] && { mkdir -p $BASEDIR/$output_folder; }
+	# the log folder
+	#LOGFOLDER="logs"
+	config_parser $CONFIGFILE LOGFOLDER 
 
-#check if source folder exists
-[ ! -d $BASEDIR/$source_folder ] && { echo "Cannot find source folder. Exiting.."; exit 1; } 
+	#log file to write out all the information
+	#LOGFILE="service.log"
+	config_parser $CONFIGFILE LOGFILE
 
-#check if log folder exists
-[ ! -d "$BASEDIR/$LOGFOLDER" ] && { mkdir -p $BASEDIR/$LOGFOLDER; }
+	#result file
+	#RESULTFILE="output.log"
+	config_parser $CONFIGFILE RESULTFILE
 
-#check if config file exists
-[ ! -f $BASEDIR/$source_folder/$CONFIGFILE ] && { echo "Config file does not exist. Exiting"; exit 1; }
+	#pid file to kill off the daemon 
+	#PIDFILE="service.pid"
+	config_parser $CONFIGFILE PIDFILE
 
-#move to base dir folder in case this script is triggered remotely from another place
-cd $BASEDIR
+	#filename
+	filename=""
 
+	#config file
+	#CONFIGFILE="fimmda.properties"
+
+	#MDIT folder and MDIT commands
+	config_parser $CONFIGFILE mdit_folder
+	config_parser $CONFIGFILE mdit_command
+	#mdit_folder="../"
+	#mdit_command="./marketdataInterface.sh -f "
+	
+	######################################
+	#check and create input folder if no exist
+	[ ! -d $BASEDIR/$input_folder ] && { mkdir -p $BASEDIR/$input_folder; }
+
+	#check and create output folder if no exist
+	[ ! -d $BASEDIR/$output_folder ] && { mkdir -p $BASEDIR/$output_folder; }
+
+	#check if source folder exists
+	[ ! -d $BASEDIR/$source_folder ] && { echo "Cannot find source folder. Exiting.."; exit 1; } 
+
+	#check if log folder exists
+	[ ! -d "$BASEDIR/$LOGFOLDER" ] && { mkdir -p $BASEDIR/$LOGFOLDER; }
+
+	#check if config file exists
+	[ ! -f $BASEDIR/$CONFIGFILE ] && { echo "Config file does not exist. Exiting"; exit 1; }
+
+	
+}
 #================================================
 #Function write log
 function writeToLog() {
@@ -84,6 +107,7 @@ function run_mdit() {
 	
 	#run the command
 	TEMPFILE=/tmp/fimmda_log_$$.log
+	echo $BASEDIR/$mdit_folder/$mdit_command $1 3>&1 2>&1 1>&3 | tee -a $TEMPFILE
 	$BASEDIR/$mdit_folder/$mdit_command $1 3>&1 2>&1 1>&3 | tee -a $TEMPFILE
 	
 	LASTPID=$!
@@ -139,7 +163,7 @@ function run_fimmda() {
 	#Set IFS setting is to prevent the space in the filename
 	SAVEIFS=$IFS
 	IFS=$(echo -en "\n\b")
-
+	
 	#Loop through the input folder and run transformation
 	#output file will be in output folder
 	writeToLog "Start looping and transforming all the files"
@@ -147,8 +171,8 @@ function run_fimmda() {
 	do
 		#for file in `ls $BASEDIR/$input_folder/*.csv`
 		#search for all csv files inside input folder
-		writeToLog "Searching input file in  $BASEDIR/$input_folder/"
-		for file in `find $BASEDIR/$input_folder/ -name '*.csv'`
+		writeToLog "Searching input file in  $BASEDIR/$input_folder"
+		for file in `find $BASEDIR/$input_folder -name '*.csv'`
 		do
 			#getting the file name only
 			filename=`basename $file`
@@ -163,8 +187,8 @@ function run_fimmda() {
 			rm $file 
 			
 			#scan the output folder and move all the newly created file to MDIT
-			writeToLog "Searching output file in  $BASEDIR/$output_folder/"
-			for file2 in `find $BASEDIR/$output_folder/ -name '*.csv'`
+			writeToLog "Searching output file in  $BASEDIR/$output_folder"
+			for file2 in `find $BASEDIR/$output_folder -name '*.csv'`
 			do
 				#move the transformed file to mdit input folder
 				filename2=`basename $file2`
@@ -184,7 +208,14 @@ function run_fimmda() {
 	IFS=$SAVEIFS
 }
 ########################################################
-#main programm
+#main program
+
+#load all variables
+load_variables
+
+#move to base dir folder in case this script is triggered remotely from another place
+cd $BASEDIR
+
 case "$1" in
       --daemon)
 			#write output, error and everythihng to log file
--- fimmda/stop_service.sh
+++ fimmda/stop_service.sh
@@ -1,33 +1,118 @@
 #!/bin/bash
+##############################
 if [ "$OS_TYPE" = "SunOS" ]; then
 	awk_command="nawk" #if it is solaris
 else
 	awk_command="awk" #if this is cygwin
 fi
+##############################
+config_parser()
+{
+	local search_string=$2
+	local properties_file=$1
+	local __resultvar=$2	
+	local myresult=$($awk_command -F "=" '/'$search_string'/ {sub(/^[ \t\r\n]+/, "", $2); sub(/[ \t\r\n]+$/, "", $2);print $2}' $properties_file)
+	eval $__resultvar="'$myresult'"
+}
 
-BASEDIR=$(dirname "$0")
+##############################
+#config file
+CONFIGFILE="sources/fimmda.properties"
+
+load_variables()
+{
+	#the current working folder 
+	BASEDIR=$(dirname $0)
+	
+	#time each loop in seconds, by default it is 60 seconds
+	config_parser $CONFIGFILE LOOPTIME 
+	
+	#input folder where we scan and pick up the file
+	#input_folder="input"
+	config_parser $CONFIGFILE input_folder
+	#output folder where we retrieve the transformed file
+	#output_folder="output"
+	config_parser $CONFIGFILE output_folder
+
+	#the source folder where the main.py resides
+	#source_folder="./sources"
+	config_parser $CONFIGFILE source_folder 
+
+	# the log folder
+	#LOGFOLDER="logs"
+	config_parser $CONFIGFILE LOGFOLDER 
+
+	#log file to write out all the information
+	#LOGFILE="service.log"
+	config_parser $CONFIGFILE LOGFILE
+
+	#result file
+	#RESULTFILE="output.log"
+	config_parser $CONFIGFILE RESULTFILE
+
+	#pid file to kill off the daemon 
+	#PIDFILE="service.pid"
+	config_parser $CONFIGFILE PIDFILE
+
+	#filename
+	filename=""
+
+	#config file
+	#CONFIGFILE="fimmda.properties"
+
+	#MDIT folder and MDIT commands
+	config_parser $CONFIGFILE mdit_folder
+	config_parser $CONFIGFILE mdit_command
+	#mdit_folder="../"
+	#mdit_command="./marketdataInterface.sh -f "
+	
+	#archive folder
+	config_parser $CONFIGFILE archive_folder
+	######################################
+	#check and create input folder if no exist
+	[ ! -d $BASEDIR/$input_folder ] && { mkdir -p $BASEDIR/$input_folder; }
+
+	#check and create output folder if no exist
+	[ ! -d $BASEDIR/$output_folder ] && { mkdir -p $BASEDIR/$output_folder; }
+
+	#check if source folder exists
+	[ ! -d $BASEDIR/$source_folder ] && { echo "Cannot find source folder. Exiting.."; exit 1; } 
+
+	#check if log folder exists
+	[ ! -d "$BASEDIR/$LOGFOLDER" ] && { mkdir -p $BASEDIR/$LOGFOLDER; }
+
+	#check if config file exists
+	[ ! -f $BASEDIR/$CONFIGFILE ] && { echo "Config file does not exist. Exiting"; exit 1; }
+}
+###############################
+
+#load all variables
+load_variables
+cd $BASEDIR
+
+#in case of errors, output to /dev/null
 exec 2>/dev/null
 #kill the services
 echo "Killing the fimmda service"
-cat $BASEDIR/logs/service.pid| $awk_command '{ system("kill -TERM -"$1);}'
-ps -ef| grep start_fimmda| grep -v grep | $awk_command '{ system("kill -9 "$2);}'
+cat $BASEDIR/$LOGFOLDER/$PIDFILE| $awk_command '{ system("kill -TERM -"$1);}'
+ps -ef| grep start_service| grep -v grep | $awk_command '{ system("kill -9 "$2);}'
 
 # do housekeeping, zip the log file
 echo "Housekeep the log file"
 current_time=$(date +"%Y%m%d_%H%M%S")
-zip $BASEDIR/logs/service_$current_time.zip $(dirname "$0")/logs/*.log
-> $BASEDIR/logs/service.log
-> $BASEDIR/logs/output.log
+zip $BASEDIR/$LOGFOLDER/service_$current_time.zip $(dirname "$0")/logs/*.log
+> $BASEDIR/$LOGFOLDER/$LOGFILE
+> $BASEDIR/$LOGFOLDER/$RESULTFILE
 
 
 # do housekeeping, zip all the archive folder
 echo "Zipping all the output files for housekeeping"
 current_time=$(date +"%Y%m%d_%H%M%S")
-find $BASEDIR/archive/* -type d -exec zip $BASEDIR/archive/archive_$current_time.zip {} +
-find $BASEDIR/archive/* -type d -exec rm -rf {} +
+find $BASEDIR/$archive_folder/* -type d -exec zip $BASEDIR/$archive_folder/archive_$current_time.zip {} +
+find $BASEDIR/$archive_folder/* -type d -exec rm -rf {} +
 
 #empty the service.pid
-> $BASEDIR/logs/service.pid 
+> $BASEDIR/$LOGFOLDER/$PIDFILE
 
 
 
